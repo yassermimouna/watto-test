cd503730a062167c20b3f9c1ef21b3caca7223b4
yasser <yesser.mimouna20@gmail.com>
2022-05-16 22:24:20 +0100

    AddingFrontend

--- Frontend/pages/user.tsx
+++
@@ -1,9 +0,0 @@
-import type { NextPage } from 'next'
-import Head from 'next/head'
-import Image from 'next/image'
-import styles from '../styles/Home.module.css'
-
-const User: NextPage = () => {
-  return (<h1>Cars page</h1>)}
-
-  export default User;
---
+++ frontend/pages/api/auth/[...nextauth].js
@@ -0,0 +1,14 @@
+import NextAuth from "next-auth"
+import providers from "next-auth/providers"
+
+export default NextAuth({
+
+  providers: [
+    providers.GitHub({
+      clientId: process.env.GITHUB_ID,
+      clientSecret: process.env.GITHUB_SECRET,
+    }),
+  
+  ],
+
+})
